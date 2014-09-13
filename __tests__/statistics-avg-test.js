4b3c0ead51fe9462caaa8c926c57efd8e19fe935
Rylan <rylans@gmail.com>
2014-09-12 21:14:17 -0700

    Log messages in test suite

--- __tests__/statistics-avg-test.js
+++ __tests__/statistics-avg-test.js
@@ -2,22 +2,37 @@ jest.autoMockOff();
 
 var fs = require('fs')
   , Log = require('log')
-  , log = new Log('info', fs.createWriteStream('diff-test.log'));
+  , log = new Log('info', fs.createWriteStream('statistics-avg-test.log'));
 
-describe('Average', function() {
- it('four numbers to equal 2.0', function() {
-   var stats = require('../statistics');
-   var avg = stats.avg;
-   var list = [1.2, 4.4, 1.1, 1.3];
+describe('avg', function() {
+  it('produces four numbers to equal 2.0', function() {
+    log.info('SPEC: avg produces four numbers to equal 2.0');
+ 
+    var stats = require('../statistics');
+    var avg = stats.avg;
+
+    var list = [1.2, 4.4, 1.1, 1.3];
+    var expected = 2.0;
+    var actual = avg(list);
+   
+    log.info("Input: " + list);
+    log.info("Expected: " + expected);
+    log.info("Actual: " + actual);
   
-   expect(avg(list)).toBeCloseTo(2.0, 12);
+    expect(actual).toBeCloseTo(expected, 12);
  });
  
- it('throws error due to empty list', function(){
-   var stats = require('../statistics');
-   var avg = stats.avg;
-   var list = [];
-  
-   expect(function() {avg(list);}).toThrow(new Error("List length is zero"));
+  it('throws error due to empty list', function(){
+    log.info('SPEC: throws error due to empty list');
+
+    var stats = require('../statistics');
+    var avg = stats.avg;
+
+    var list = [];
+	var expected_err = "List length is zero";
+	
+	log.info("Expected error: " + expected_err);
+
+    expect(function() {avg(list);}).toThrow(new Error(expected_err));
  });
 });
--- vector.js
+++ vector.js
@@ -4,22 +4,19 @@
 * Rylan Santinon
 */
 
-var fs = require('fs')
-  , Log = require('log')
-  , log = new Log('info', fs.createWriteStream('testv.log'));
-
 function diff(vec){
-  log.error("diff() being called\n");
   return diff_lag(vec, 1);
 }
 
 function diff_lag(vec, lag){
-  log.error("diff( , ) being called\n");
   var vlen = vec.length;
+  
+  if(lag <= 0) throw new Error("Lag must be greater than zero");
   if(vlen <= lag) throw new Error("Vector has too few elements");
+ 
   var diff_vector = [];
   for(var i = 0; i < vlen - lag; i++){
-    diff_vector.push(vec[i+1] - vec[i]);
+    diff_vector.push(vec[i + lag] - vec[i]);
   }
   return diff_vector;
 }
