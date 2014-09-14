1ae14e6739c41d83be81ef1acb78219f2f24151e
Rylan <rylans@gmail.com>
2014-09-14 12:08:15 -0700

    Add standard deviation function and test

---
+++ __tests__/statistics-sd-test.js
@@ -0,0 +1,24 @@
+jest.autoMockOff();
+
+var fs = require('fs')
+  , Log = require('log')
+  , log = new Log('info', fs.createWriteStream('statistics-sd-test.log'));
+  
+describe('sd', function(){
+  it('produces correct values', function(){
+    log.info('SPEC: sd produces correct values');
+
+    var stats = require('../statistics');
+	var sd = stats.sd;
+	
+	var list = [206,76,-224,36,-94];
+	var expected = 147.32277488562318;
+	var actual = sd(list);
+
+	log.info("Input: " + list);
+	log.info("Expected: " + expected);
+	log.info("Actual: " + actual);
+
+	expect(actual).toBeCloseTo(expected, 12);
+  });
+});
--- statistics.js
+++ statistics.js
@@ -4,7 +4,9 @@
 * Rylan Santinon
 */
 
-//sample mean
+/**
+* Calculates the arithmetic mean
+*/
 function avg(list){
   if(list.length === 0) throw new Error("List length is zero");
   var avg = 0.0;
@@ -14,11 +16,13 @@ function avg(list){
   return (avg/list.length);
 }
 
-//variance
+/**
+* Calculates the variance
+*/
 function variance(list){
   if(list.length === 0) throw new Error("List length is zero");
   var m = avg(list);
-  var sum = 0;
+  var sum = 0.0;
   for(var i = 0; i < list.length; i++){
     sum = sum + (list[i] - m) * (list[i] - m);
   }
@@ -27,7 +31,17 @@ function variance(list){
   if(variance < 0) throw new Error("Variance is never negative");
   return variance;
 }
+
+function standard_deviation(list){
+  return Math.sqrt(variance(list));
+}
+
 module.exports = {
+  mean: avg,
   avg: avg,
-  variance: variance
+  
+  variance: variance,
+ 
+  sd: standard_deviation,
+  standard_deviation: standard_deviation
 };
