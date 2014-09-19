cfedad6b039b11196af785686283127397da2d05
Rylan <rylans@gmail.com>
2014-09-18 20:01:34 -0700

    Add another test for minus()

--- __tests__/timeseries-minus-test.js
+++ __tests__/timeseries-minus-test.js
@@ -23,4 +23,21 @@ describe('minus', function(){
 
 	expect(actual).toEqual(expected);
   });
+  
+  it('returns correct values for real times', function(){
+    log.info('SPEC: minus returns correct values for real times');
+	
+	var list1 = [9, 5, 2, 1, 14];
+	var list2 = [2, -4, 9, 4, 1];
+	var ts1 = ts.Timeseries(list1, null);
+	var ts2 = ts.Timeseries(list2, [5,10,15,20,25]);
+	var actual = ts1.minus(ts2);
+	var expected = ts.Timeseries([7, 9, -7, -3, 13], [5,10,15,20,25]);
+
+	log.info("Input: " + list1 + " " + list2);
+	log.info("Expected: " + expected);
+	log.info("Actual: " + actual);
+
+	expect(actual).toEqual(expected);
+  });
 });
